9f8eee131a75c2b96ae41ecdc8800ef65124540d
Andrew <andrewfairley95@gmail.com>
2022-06-21 07:28:14 +0100

    added a simple loop

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,6 +1,7 @@
 number_1 = 44
 number_2 = 55
 
+sum = number_1 * number_2
 
 my_string_1 = "hello"
 my_string_2 = "world"
@@ -17,3 +18,9 @@ print(greeting.capitalize())
 
 
 
+counter = 10
+target = 0
+
+while counter >= target:
+  print(counter)
+  counter -= 1
